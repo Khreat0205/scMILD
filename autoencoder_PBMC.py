bfa34fd7cd8baab9824ef5011ba355165bc6b7ef
Khreat0205 <scientist0205@snu.ac.kr>
2024-05-14 02:15:50 +0000

    Add run PBMC / Lupus

---
+++ autoencoder_PBMC.py
@@ -0,0 +1,187 @@
+
+import sys
+import os 
+import torch
+
+from torch import nn
+from torch.utils.data import DataLoader, ConcatDataset
+import numpy as np
+import argparse
+import json
+import random
+import copy
+from utils import *
+from dataset import *
+from model import *
+from tqdm import tqdm as tdqm
+import time
+
+ae_learning_rate = 1e-3
+ae_epochs = 250
+ae_patience = 15
+ae_latent_dim = 128
+ae_hidden_layers = [512, 256, 128]
+ae_batch_size = 128
+
+hyperparameters_dict = {
+    "ae_learning_rate": ae_learning_rate,
+    "ae_epochs": ae_epochs,
+    "ae_patience": ae_patience,
+    "ae_latent_dim": ae_latent_dim,
+    "ae_hidden_layers": ae_hidden_layers,
+    "ae_batch_size": ae_batch_size
+}
+
+
+# dir_path="lupus/disease"
+base_path = f"data/PBMC"
+target_dir = f'{base_path}/AE/'
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir, exist_ok=False)
+
+hyperparameter_file_path = os.path.join(target_dir, 'hyperparameters_ae.json')
+if not os.path.exists(hyperparameter_file_path):
+    with open(hyperparameter_file_path, 'w') as file:
+        json.dump(hyperparameters_dict, file, indent=4)
+        
+device_num = 3
+device = torch.device(f'cuda:{device_num}' if torch.cuda.is_available() else 'cpu')
+print("INFO: Using device: {}".format(device))
+
+
+def negative_binomial_loss(y_pred, theta, y_true, eps=1e-10):
+    # adapted from https://github.com/uhlerlab/STACI/blob/master/gae/gae/optimizer.py
+    nbloss1=torch.lgamma(theta+eps) + torch.lgamma(y_true+1.0) - torch.lgamma(y_true+theta+eps)
+    nbloss2=(theta+y_true) * torch.log(1.0 + (y_pred/(theta+eps))) + (y_true * (torch.log(theta+eps) - torch.log(y_pred+eps)))
+    nbloss=nbloss1+nbloss2
+    return torch.mean(nbloss)
+
+
+
+def _train_or_test(model=None, dataloader=None, optimizer=None, device='cuda'):
+    is_train = optimizer is not None
+    model.train() if is_train else model.eval()
+
+    start = time.time()
+    
+    n_batches = 0
+    
+    total_recons_loss = 0
+    for i, (data, _, instance_labels) in enumerate(dataloader):
+        input_data = data.to(device)
+        target = instance_labels.to(device)
+        grad_req = torch.enable_grad() if is_train else torch.no_grad()
+        with grad_req:
+            mu_recon, theta_recon = model(input_data, target, is_train=is_train)
+            recons_loss = negative_binomial_loss(mu_recon,theta_recon, input_data)
+           
+            # Accumulate loss values
+            n_batches += 1
+            
+            total_recons_loss += recons_loss.item()
+            if is_train:
+                optimizer.zero_grad()
+                recons_loss.backward()
+                optimizer.step()
+
+            del input_data
+            del mu_recon
+            del theta_recon
+
+    end = time.time()
+    
+    avg_recons_loss = total_recons_loss / n_batches
+    
+    # Print performance metrics
+    print(f'\tTime: {end - start:.2f}s')
+    print(f'\tReconstruction Loss: {avg_recons_loss:.4f}')
+    
+    return avg_recons_loss
+
+
+def train(model, optimizer=None, dataloader=None, device='cuda'):
+    assert(optimizer is not None)
+    print('\ttrain')
+    model.train()
+    return _train_or_test(model, optimizer=optimizer, dataloader=dataloader,device=device)
+
+
+def test(model, optimizer=None, dataloader=None, device='cuda', csv_path=None):
+    print('\ttest')
+    model.eval()
+    performance_metrics = _train_or_test(model, optimizer=optimizer, dataloader=dataloader,device=device)
+    if csv_path is not None:
+        data = {
+            'Reconstruction Loss': [performance_metrics],
+        }
+        df = pandas.DataFrame(data)
+        if not os.path.isfile(csv_path):
+            df.to_csv(csv_path, index=False)
+        else:
+            df.to_csv(csv_path, mode='a', header=False, index=False)
+            
+    return performance_metrics
+
+
+def train_ae(ae, train_dl, val_dl, optimizer, device, n_epochs=25, patience= 10, model_save_path='best_ae.pth'):
+    best_loss = float('inf')
+    no_improvement = 0
+    
+    for epoch in range(n_epochs):
+        print('epoch: \t{0}'.format(epoch))
+        train_recon = train(model=ae, dataloader=train_dl,optimizer=optimizer, device=device)
+        # train_recon_addition = train(model=ae, dataloader=train_dl_addition,optimizer=optimizer, device=device)
+        print('\n')
+        val_recon = test(model=ae, dataloader=val_dl, device=device)
+        avg_val_loss = val_recon
+        
+        if avg_val_loss < best_loss:
+            best_loss = avg_val_loss
+            best_model_wts = copy.deepcopy(ae.state_dict())
+            torch.save(best_model_wts, model_save_path)
+            no_improvement = 0
+            
+        else:
+            no_improvement += 1
+            if no_improvement == patience:
+                break
+    ae.load_state_dict(best_model_wts)  
+    
+    return ae
+
+
+
+for exp in tdqm(range(1,9),  desc="Experiment"):
+    ################################## Load Dataset - Instance ###############
+    train_dataset, val_dataset, test_dataset, label_encoder = load_dataset_and_preprocessors(base_path, exp, device)
+    
+    train_dl = DataLoader(train_dataset, batch_size=ae_batch_size, shuffle=False, drop_last=False)
+    val_dl = DataLoader(val_dataset, batch_size=round(ae_batch_size/2), shuffle=False, drop_last=False)
+    test_dl = DataLoader(test_dataset, batch_size=round(ae_batch_size/2), shuffle=False, drop_last=False)
+    del train_dataset, val_dataset, test_dataset
+    
+    torch.manual_seed(exp)
+    torch.cuda.manual_seed(exp)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(exp)
+    random.seed(exp)
+    torch.cuda.manual_seed_all(exp)
+    
+    ################################## Set Encoding Model ####################
+    ## PBMC, UC, 2000 HVGs / Lupus, 3000 HVGs
+    ae = AENB(input_dim=2000, latent_dim=ae_latent_dim, 
+                        device=device, hidden_layers=ae_hidden_layers, 
+                        activation_function=nn.Sigmoid).to(device)
+
+    ae_optimizer = torch.optim.Adam(ae.parameters(), lr= ae_learning_rate)
+    ################################## Training VAE ####################
+    ae = train_ae(ae, train_dl, val_dl, ae_optimizer, device, n_epochs=ae_epochs, patience= ae_patience, model_save_path=f"{target_dir}/aenb_{exp}.pth")
+
+
+    test_recon = test(model=ae, optimizer=None, dataloader=test_dl, device=device, csv_path=f"{target_dir}/aenb_test.csv")
+    
+    del train_dl, val_dl, test_dl, ae, ae_optimizer, test_recon
+    torch.cuda.empty_cache()
+    
+
---
+++ run_scMILD_NS copy.py
@@ -0,0 +1,118 @@
+import sys
+import os 
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import numpy as np
+import argparse
+import json
+import random
+from utils import *
+from dataset import *
+from model import *
+from optimizer import *
+
+from torch.utils.tensorboard import SummaryWriter
+# from sklearn.metrics import roc_auc_score, accuracy_score, precision_recall_curve, auc
+base_path = 'data/NS'
+ae_dir = f'{base_path}/AE/'
+
+device_num = 3
+device = torch.device(f'cuda:{device_num}' if torch.cuda.is_available() else 'cpu')
+print("INFO: Using device: {}".format(device))
+
+hyperparameter_file_path = os.path.join(ae_dir, f'hyperparameters_ae.json')
+
+with open(hyperparameter_file_path, 'r') as file:
+    loaded_args_dict = json.load(file)       
+    
+loaded_args = argparse.Namespace(**loaded_args_dict)
+
+
+ae_learning_rate = loaded_args.ae_learning_rate
+ae_epochs = loaded_args.ae_epochs
+ae_patience = loaded_args.ae_patience
+ae_latent_dim = loaded_args.ae_latent_dim
+ae_hidden_layers = loaded_args.ae_hidden_layers
+ae_batch_size = loaded_args.ae_batch_size
+
+
+for exp in range(1, 9):
+    print(f'Experiment {exp}')
+    train_dataset, val_dataset, test_dataset, label_encoder = load_dataset_and_preprocessors(base_path, exp, device)
+
+    instance_train_dataset = update_instance_labels_with_bag_labels(train_dataset, device=device)
+    instance_val_dataset = update_instance_labels_with_bag_labels(val_dataset, device=device)
+    instance_test_dataset = update_instance_labels_with_bag_labels(test_dataset, device=device)
+    
+    torch.manual_seed(exp)
+    torch.cuda.manual_seed(exp)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(exp)
+    random.seed(exp)
+    torch.cuda.manual_seed_all(exp)
+
+    vae_batch_size = 512
+    instance_train_dl = DataLoader(instance_train_dataset, batch_size=vae_batch_size, shuffle=True, drop_last=False)
+    instance_val_dl = DataLoader(instance_val_dataset, batch_size=vae_batch_size, shuffle=True, drop_last=False)
+    instance_test_dl = DataLoader(instance_test_dataset, batch_size=round(vae_batch_size/2), shuffle=False, drop_last=False)
+
+    bag_train = MilDataset(train_dataset.data.to(device), train_dataset.ids.unsqueeze(0).to(device), train_dataset.labels.to(device), train_dataset.instance_labels.to(device))
+    bag_val = MilDataset(val_dataset.data.to(device), val_dataset.ids.unsqueeze(0).to(device), val_dataset.labels.to(device), val_dataset.instance_labels.to(device))
+    bag_test = MilDataset(test_dataset.data.to(device), test_dataset.ids.unsqueeze(0).to(device), test_dataset.labels.to(device), test_dataset.instance_labels.to(device))
+
+    bag_train_dl = DataLoader(bag_train,batch_size = 14, shuffle=False, drop_last=False,collate_fn=collate)
+    bag_val_dl = DataLoader(bag_val,batch_size = 15, shuffle=False, drop_last=False,collate_fn=collate)
+    bag_test_dl = DataLoader(bag_test,batch_size = 15, shuffle=False, drop_last=False,collate_fn=collate)
+
+
+    
+
+    ae = AENB(input_dim=3000, latent_dim=ae_latent_dim, 
+                            device=device, hidden_layers=ae_hidden_layers, 
+                            activation_function=nn.Sigmoid).to(device)
+    ae.load_state_dict(torch.load(f"{ae_dir}/aenb_{exp}.pth"))
+    
+
+    mil_latent_dim = 64
+    # mil_learning_rate = 1e-3
+    # mode 1
+    # attention_module = AttentionModule(L=vae_latent_dim, D=vae_latent_dim //4 , K=1).to(device)
+    # mode 2
+    encoder_dim = ae_latent_dim
+    model_encoder = ae.features
+    # encoder_dim = 64
+    # model_encoder = EncoderBranch(proto_vae, encoder_dim, activation_function=nn.LeakyReLU).to(device)
+    attention_module = AttentionModule(L=encoder_dim, D=encoder_dim, K=1).to(device)
+    # attention_module = GatedAttentionModule(L=encoder_dim, D=encoder_dim, K=1).to(device)
+
+    model_teacher = TeacherBranch(input_dims = encoder_dim, latent_dims=mil_latent_dim, 
+                            attention_module=attention_module, num_classes=2, activation_function=nn.Tanh)
+
+    model_student = StudentBranch(input_dims = encoder_dim, latent_dims=mil_latent_dim, num_classes=2, activation_function=nn.Tanh)
+     
+    
+    # model_encoder.to(device)
+    model_teacher.to(device)
+    model_student.to(device)
+    teacher_learning_rate = 1e-4
+    student_learning_rate = 1e-3
+    encoder_learning_rate = 1e-3
+    optimizer_teacher = torch.optim.Adam(model_teacher.parameters(), lr=teacher_learning_rate)
+
+    optimizer_student = torch.optim.Adam(model_student.parameters(), lr=student_learning_rate)
+
+    optimizer_encoder = torch.optim.Adam(model_encoder.parameters(), lr=encoder_learning_rate)
+    ### 지금 이 아래 세팅이 best model 
+    scMILD_epoch = 500
+    scMILD_neg_weight = 0.3
+    scMILD_stuOpt = 3
+    scMILD_patience = 15
+    add_suffix = "reported"
+    exp_writer = SummaryWriter(f'runs/NS')
+    #default patience = 15 
+    test_optimizer= Optimizer(exp, model_teacher, model_student, model_encoder, optimizer_teacher, optimizer_student, optimizer_encoder, bag_train_dl, bag_val_dl, bag_test_dl, instance_train_dl, instance_val_dl, instance_test_dl,  scMILD_epoch, device, val_combined_metric=False, stuOptPeriod=scMILD_stuOpt,stu_loss_weight_neg= scMILD_neg_weight, writer=exp_writer,
+                              patience=scMILD_patience, csv=f'results/NS_ae_ed{encoder_dim}_md{mil_latent_dim}_lr{teacher_learning_rate}_{scMILD_epoch}_{scMILD_neg_weight}_{scMILD_stuOpt}_{scMILD_patience}_{add_suffix}.csv', saved_path=f'results/model_NS_ae_ed{encoder_dim}_md{mil_latent_dim}_lr{teacher_learning_rate}_{scMILD_epoch}_{scMILD_neg_weight}_{scMILD_stuOpt}_{scMILD_patience}_{add_suffix}',epoch_warmup=0)
+    test_optimizer.optimize()
+    print(test_optimizer.evaluate_teacher(400, test=True))
